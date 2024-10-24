b4e436ede4e80ff096e8f882bcfa0225abe6a4b1
Andre de Paula <andrepps22@gmail.com>
2024-10-24 14:00:19 +0000

    configurando gunicorn e retirando o put e delete do corretor.

--- api/v1/endpoint/corretor.py
+++ api/v1/endpoint/corretor.py
@@ -55,51 +55,51 @@ async def get_corretor(id: int, db: AsyncSession = Depends(get_session)):
     return corretores
 
 
-@router.put('/corretor/{id_corretor}', response_model=CorretorSchema, tags=['Corretor'], status_code=status.HTTP_201_CREATED)
-async def put_corretor(db: AsyncSession = Depends(get_session), corretor: CorretorSchema = CorretorSchema):
+# @router.put('/corretor/{id_corretor}', response_model=CorretorSchema, tags=['Corretor'], status_code=status.HTTP_201_CREATED)
+# async def put_corretor(db: AsyncSession = Depends(get_session), corretor: CorretorSchema = CorretorSchema):
 
-    async with db as session:
-        query = select(CorretorModel).where(CorretorModel.id_corretor == id)
-        result = await session.execute(query)
-        user = result.scalars().one_or_none()
+#     async with db as session:
+#         query = select(CorretorModel).where(CorretorModel.id_corretor == id)
+#         result = await session.execute(query)
+#         user = result.scalars().one_or_none()
 
-        if user:
-            if user.username:
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail='Usuário já esta sendo utilizado'
-                )
-            elif user.email:
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail='Email já esta sendo utilizado'
-                )
-            sql = update(CorretorModel).values(
-                nome=corretor.nome,
-                cpf=corretor.cpf,
-                endereco=corretor.endereco,
-                celular=corretor.celular,
-                data_nascimento=corretor.data_nascimento).where(CorretorModel.id_corretor == id)
+#         if user:
+#             if user.username:
+#                 raise HTTPException(
+#                     status_code=status.HTTP_409_CONFLICT,
+#                     detail='Usuário já esta sendo utilizado'
+#                 )
+#             elif user.email:
+#                 raise HTTPException(
+#                     status_code=status.HTTP_409_CONFLICT,
+#                     detail='Email já esta sendo utilizado'
+#                 )
+#             sql = update(CorretorModel).values(
+#                 nome=corretor.nome,
+#                 cpf=corretor.cpf,
+#                 endereco=corretor.endereco,
+#                 celular=corretor.celular,
+#                 data_nascimento=corretor.data_nascimento).where(CorretorModel.id_corretor == id)
 
-            await session.execute(sql)
-            await session.commit()
-            return corretor
-        else:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail='Por favor verifique os campos e tente novamente.')
+#             await session.execute(sql)
+#             await session.commit()
+#             return corretor
+#         else:
+#             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+#                                 detail='Por favor verifique os campos e tente novamente.')
 
 
-@router.delete('/corretor/{id_corretor}', status_code=status.HTTP_204_NO_CONTENT, tags=['Corretor'])
-async def delete_corretor(id: int, db: AsyncSession = Depends(get_session)):
-    async with db as session:
-        sql = select(CorretorModel).where(CorretorModel.id_corretor == id)
-        result = await session.execute(sql)
-        user = result.scalars().one_or_none()
+# @router.delete('/corretor/{id_corretor}', status_code=status.HTTP_204_NO_CONTENT, tags=['Corretor'])
+# async def delete_corretor(id: int, db: AsyncSession = Depends(get_session)):
+#     async with db as session:
+#         sql = select(CorretorModel).where(CorretorModel.id_corretor == id)
+#         result = await session.execute(sql)
+#         user = result.scalars().one_or_none()
 
-        if user:
-            sql = delete(CorretorModel).where(CorretorModel.id_corretor == id)
-            await session.execute(sql)
-            await session.commit()
-        else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f'Não exites nenhum corretor com o id: {id}')
+#         if user:
+#             sql = delete(CorretorModel).where(CorretorModel.id_corretor == id)
+#             await session.execute(sql)
+#             await session.commit()
+#         else:
+#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                                 detail=f'Não exites nenhum corretor com o id: {id}')
---
+++ gunicorn_conf.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+max_requests = 1000
+
+max_requests_jitter = 50
+
+loglevel="info"
+
+accesslog = "/home/ubuntu/apisinapse/api_automacao_sinapse/logs/access.log"
+errorlog = "/home/ubuntu/apisinapse/api_automacao_sinapse/logs/error.log"
+
+bind = "0.0.0.0:8000"
+
+worker_class = "uvicorn.workers.UvicornWorker"
+
+workers = (multiprocessing.cpu_count() * 2) + 1
