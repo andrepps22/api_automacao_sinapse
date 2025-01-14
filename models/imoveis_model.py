28961fe58c3b818468f34896dc5260c143228cc1
Andre de Paula <andrepps22@gmail.com>
2025-01-14 07:07:11 -0300

    Alteração dados de imoveis

--- models/imoveis_model.py
+++ models/imoveis_model.py
@@ -1,6 +1,7 @@
 from core.config import setting
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey, Integer, String, JSON, Boolean
+from sqlalchemy import ForeignKey, Integer, String, JSON, DateTime, func
+from datetime import  datetime
 from typing import Dict
 
 
@@ -12,22 +13,54 @@ reg = setting.DBModelReg
 @reg.mapped_as_dataclass()
 class ImoveisModel:
     __tablename__= 'imovel'
-    
-    codigo_imovel: Mapped[str] = mapped_column(String, primary_key=True)
-    codigo_corretor: Mapped[str] = mapped_column(String)
-    nome_proprietario: Mapped[str] = mapped_column(String)
-    tipo_imovel: Mapped[str] = mapped_column(String)
-    condominio: Mapped[bool] = mapped_column(Boolean)
+
+
+    codigoImovel: Mapped[str] = mapped_column(String, primary_key=True)
+    codigoCorretor: Mapped[str] = mapped_column(String)
+    numeroRegistro: Mapped[int] = mapped_column(Integer)
+    documentoRegistro: Mapped[str] = mapped_column(JSON)
+    numeroMatricula: Mapped[int] = mapped_column(Integer)
+    documentoMatricula: Mapped[str] = mapped_column(JSON)
+    IPTU: Mapped[str] = mapped_column(String)
+    qtdParcelasAtrasadasIPTU: Mapped[int] = mapped_column(Integer)
+    observacoesIPTU: Mapped[str] = mapped_column(String)
+    condominio: Mapped[str] = mapped_column(String)
+    qtdParcelasAtrasadasCondominio: Mapped[int] = mapped_column(Integer)
+    observacoesCondominio: Mapped[str] = mapped_column(String)
+    cep: Mapped[str] = mapped_column(String)
     rua: Mapped[str] = mapped_column(String)
     numero: Mapped[str] = mapped_column(String)
+    bairro: Mapped[str] = mapped_column(String)
+    cidade: Mapped[str] = mapped_column(String)
+    estado: Mapped[str] = mapped_column(String)
     referencia: Mapped[str] = mapped_column(String)
     descricao: Mapped[str] = mapped_column(String)
+    valorImovel: Mapped[str] = mapped_column(String)
+    condominio: Mapped[str] = mapped_column(String)
+    valorCondominio: Mapped[str] = mapped_column(String)
+    garagem: Mapped[str] = mapped_column(String)
+    garagemCoberta: Mapped[str] = mapped_column(String, nullable=True)
+    garagemTipo: Mapped[str] = mapped_column(String, nullable=True)
+    garagemFoto: Mapped[str] = mapped_column(JSON, nullable=True)
+    fachadaFoto: Mapped[str] = mapped_column(JSON)
+    salaFotos: Mapped[str] = mapped_column(JSON)
+    suite: Mapped[str] = mapped_column(String)
+    suiteFotos: Mapped[str] = mapped_column(JSON, nullable=True)
+    quantidadeQuarto: Mapped[int] = mapped_column(Integer)
     quartos: Mapped[str] = mapped_column(JSON)
     cozinha: Mapped[str] = mapped_column(JSON)
     sacada: Mapped[str] = mapped_column(JSON)
+    quantidadeBanheiros: Mapped[int] = mapped_column(Integer)
     banheiro: Mapped[str] = mapped_column(JSON)
-    observacoes: Mapped[str] = mapped_column(String)
-    cep: Mapped[str] = mapped_column(String)
+    quartos: Mapped[str] = mapped_column(JSON)
+    cozinha: Mapped[str] = mapped_column(JSON)
+    metrosQuadradoUtil: Mapped[str] = mapped_column(String)
+    metrosQuadradoTotal: Mapped[str] = mapped_column(String)
+    create_at: Mapped[datetime] = mapped_column(DateTime, init=False, server_default=func.now())
+
+    
+
+    
     
     __table_args__ = {'extend_existing': True}
     
